8052966920958e892b69ee1463366339bf21ca7f
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2023-03-06 09:46:40 +0530

    Revert "Flutter App Day 4Completed"
    This reverts commit e93e498640b07f567451e5e282603a8034e9267d.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1_from_basic/pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 import 'pages/home_page.dart';
 
 void main() {
@@ -14,12 +14,8 @@ class MyApp extends StatelessWidget {
       //  home: HomePage(),
 
       //if we want to give property in light mode
-      themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple,
-      fontFamily:GoogleFonts.lato().fontFamily,
-      // primaryTextTheme: GoogleFonts.latoTextTheme()
-      ),
-
+       themeMode:ThemeMode.light,
+       theme: ThemeData(primarySwatch: Colors.deepPurple),
 
       //it is for dark mode
       // home: HomePage(),
@@ -28,10 +24,10 @@ class MyApp extends StatelessWidget {
       //   brightness: Brightness.dark,
       // ),
 
-      initialRoute: "/login",
+      initialRoute: "/home",
       routes: {
-        "/login": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
+        "/": (context) => LoginPage(),
+        "/home":(context)=> HomePage(),
         "/login": (context) => LoginPage(),
       },
     );
